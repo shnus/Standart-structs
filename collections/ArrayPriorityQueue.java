b347adf4af6f9655feeae89341b541fbc9f1c368
Alex Antufiev <antufiev.alex@gmail.com>
2017-10-08 01:00:34 +0300

    Add files via upload

--- collections/ArrayPriorityQueue.java
+++ collections/ArrayPriorityQueue.java
@@ -1,88 +1,141 @@
-package seminar1.collections;
+package structures_and_algorythms.collections;
 
+import java.lang.reflect.Array;
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 
 public class ArrayPriorityQueue<Key extends Comparable<Key>> implements IPriorityQueue<Key> {
 
     private Key[] elementData;
     private Comparator<Key> comparator;
+    private int size;
+    private int capacity;
+    private Class<?> clazz;
 
-    public ArrayPriorityQueue() {
-        /* TODO: implement it — O(n) */
+    @SuppressWarnings("unchecked")
+    public ArrayPriorityQueue(Class<?> clazz) {
+        this.clazz = clazz;
+        elementData = (Key[]) Array.newInstance(this.clazz, 10);
+        capacity = elementData.length;
+        size = 0;
+        comparator = Comparator.naturalOrder();
     }
 
-    public ArrayPriorityQueue(Comparator<Key> comparator) {
-        /* TODO: implement it — O(n) */
+    public ArrayPriorityQueue(Key[] elementData) {
+        this.clazz = elementData.getClass().getComponentType();
+        this.elementData = elementData;
+        capacity = elementData.length;
+        size = elementData.length;
+        comparator = Comparator.naturalOrder();
+        for (int i = size - 1; i > -1; i--) {
+            int index = 2 * i + 1;
+            if (index < size) {
+                siftDown(i);
+            }
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public ArrayPriorityQueue(Comparator<Key> comparator, Key[] elementData) {
+        this(elementData);
         this.comparator = comparator;
     }
 
     @Override
     public void add(Key key) {
-        /* TODO: implement it — O(log n) */
+        if (size < capacity) {
+            elementData[size] = key;
+            siftUp(size);
+            size++;
+        } else {
+            grow();
+            add(key);
+        }
     }
 
     @Override
     public Key peek() {
-        /**
-         * TODO: implement it — O(1)
-         * Посмотреть на минимальный элемент
-         */
-        return null;
+        return elementData[0];
     }
 
     @Override
     public Key extractMin() {
-        /**
-         * TODO: implement it — O(log n)
-         * Достать минимальный элемент
-         *  и перестроить кучу
-         */
-        return null;
+        Key min = elementData[0];
+        elementData[0] = elementData[size - 1];
+        elementData[size - 1] = null;
+        size--;
+        siftDown(0);
+        shrink();
+        return min;
     }
 
     @Override
     public boolean isEmpty() {
-        /* TODO: implement it */
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        /* TODO: implement it */
-        return 0;
+        return size;
     }
 
-    private void siftUp() {
-        /**
-         * TODO: implement it — O(log n)
-         * Просеивание вверх —
-         *  подъём элемента больше родителей
-         */
+    private void swap(int first, int second) {
+        Key temp = elementData[first];
+        elementData[first] = elementData[second];
+        elementData[second] = temp;
     }
 
-    private void siftDown() {
-        /**
-         * TODO: implement it — O(log n)
-         * Просеивание вниз
-         *  спуск элемента меньше детей
-         */
+    private void siftUp(int index) {
+        if (index > 0) {
+            int fatherIndex = index % 2 == 0 ? (index - 2) / 2 : (index - 1) / 2;
+            if (fatherIndex < size && greater(fatherIndex, index)) {
+                swap(fatherIndex, index);
+                siftUp(fatherIndex);
+            }
+        }
     }
 
+    private void siftDown(int index) {
+        if (index > -1) {
+            int child1 = index * 2 + 1;
+            int child2 = index * 2 + 2;
+            if (child1 < size && greater(index, child1)) {
+                if (child2 < size && greater(index, child2)) {
+                    int min = greater(child1, child2) ? child2 : child1;
+                    swap(index, min);
+                    siftDown(min);
+                } else {
+                    swap(index, child1);
+                    siftDown(child1);
+                }
+            } else if (child2 < size && greater(index, child2)) {
+                swap(index, child2);
+                siftDown(child2);
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
     private void grow() {
-        /**
-         * TODO: implement it
-         * Если массив заполнился,
-         * то увеличить его размер в полтора раз
-         */
+        if (size == capacity) {
+            Key[] newArray = (Key[]) Array.newInstance(clazz, capacity / 2 * 3 + 1);
+            System.arraycopy(elementData, 0, newArray, 0, size);
+            elementData = newArray;
+            capacity = elementData.length;
+        }
     }
 
+    @SuppressWarnings("unchecked")
     private void shrink() {
-        /**
-         * TODO: implement it
-         * Если количество элементов в четыре раза меньше,
-         * то уменьшить его размер в два раза
-         */
+        if (4 * size <= capacity) {
+            Key[] newArray = (Key[]) Array.newInstance(clazz, capacity / 2);
+            System.arraycopy(elementData, 0, newArray, 0, size);
+            elementData = newArray;
+            capacity = elementData.length;
+        }
     }
 
     private boolean greater(int i, int j) {
@@ -94,7 +147,19 @@ public class ArrayPriorityQueue<Key extends Comparable<Key>> implements IPriorit
 
     @Override
     public Iterator<Key> iterator() {
-        /* TODO: implement it */
-        return null;
+        return new Iterator<Key>() {
+            @Override
+            public boolean hasNext() {
+                return size > 0;
+            }
+
+            @Override
+            public Key next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return extractMin();
+            }
+        };
     }
 }
